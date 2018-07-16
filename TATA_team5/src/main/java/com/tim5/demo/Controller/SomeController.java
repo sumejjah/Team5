cc3a90313b31b8b4368cf62bbbe7463dd935f0c2
megarami <nramic15@gmail.com>
2018-07-16 16:23:17 +0200

    added mainpage.

---
+++ TATA_team5/src/main/java/com/tim5/demo/Controller/SomeController.java
@@ -0,0 +1,18 @@
+package com.tim5.demo.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class SomeController {
+
+    @GetMapping("/demo/com/tim5/demo/View/index.html")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+}
+
