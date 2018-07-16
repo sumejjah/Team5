90736f634a8c21994ddffb61c87483c697661f14
unknown <b8emir@gmail.com>
2018-07-16 12:28:35 +0200

    Initial app

---
+++ TATA_team5/src/main/java/com/tim5/demo/TravelActivityTrackerApplication.java
@@ -0,0 +1,14 @@
+package com.tim5.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.print.StreamPrintService;
+
+@SpringBootApplication
+public class TravelActivityTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelActivityTrackerApplication.class, args);
+    }
+}
