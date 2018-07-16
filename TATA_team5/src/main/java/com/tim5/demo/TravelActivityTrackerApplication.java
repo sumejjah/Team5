ef9e83ff55b172e812f399e2361fcb39683a7cca
megarami <nramic15@gmail.com>
2018-07-16 13:48:51 +0200

    added springframework

--- TATA_team5/src/main/java/com/tim5/demo/TravelActivityTrackerApplication.java
+++ TATA_team5/src/main/java/com/tim5/demo/TravelActivityTrackerApplication.java
@@ -1,14 +1,31 @@
 package com.tim5.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import javax.print.StreamPrintService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import java.util.Arrays;
 
 @SpringBootApplication
+
 public class TravelActivityTrackerApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(TravelActivityTrackerApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx){
+        return args->{
+            System.out.println("Let's inspect the beans provided by Spring boot");
+            String[] beanNames=ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for(String beanName : beanNames){
+                System.out.println(beanName);
+            }
+
+        };
     }
 }
