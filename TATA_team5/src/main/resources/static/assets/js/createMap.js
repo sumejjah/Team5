af31a2d29674caa8fd5c257a4cac99264b065db6
megarami <nramic15@gmail.com>
2018-07-17 17:07:10 +0200

    moved scripts to js folder. not working properly, will fix in the morning

---
+++ TATA_team5/src/main/resources/static/assets/js/createMap.js
@@ -0,0 +1,10 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoibWVnYXJhbWkiLCJhIjoiY2pqcGtxdmJ5N3llcjNrcDEyY2UxdWZyZyJ9.aPCrM0aKAkSEXfcW2c0EOw';
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v10',
+    center: [18.4131,43.8563],
+    zoom: 15
+});
+map.on('load', function() {
+    getRoute();
+});
---
+++ TATA_team5/src/main/resources/static/assets/js/map.js
@@ -0,0 +1,54 @@
+function getRoute() {
+    var start = [18.4128671, 43.8568503];
+    var end = [17.4307913, 43.8594828];
+    var directionsRequest = 'https://api.mapbox.com/directions/v5/mapbox/driving/' + start[0] + ',' + start[1] + ';' + end[0] + ',' + end[1] + '?steps=true&geometries=geojson&access_token=' + mapboxgl.accessToken;
+    $.ajax({
+        method: 'GET',
+        url: directionsRequest,
+    }).done(function(data) {
+        var route = data.routes[0].geometry;
+        map.addLayer({
+            id: 'route',
+            type: 'line',
+            source: {
+                type: 'geojson',
+                data: {
+                    type: 'Feature',
+                    geometry: route
+                }
+            },
+            paint: {
+                'line-width': 2
+            }
+        });
+        map.addLayer({
+            id: 'start',
+            type: 'circle',
+            source: {
+                type: 'geojson',
+                data: {
+                    type: 'Feature',
+                    geometry: {
+                        type: 'Point',
+                        coordinates: start
+                    }
+                }
+            }
+        });
+        map.addLayer({
+            id: 'end',
+            type: 'circle',
+            source: {
+                type: 'geojson',
+                data: {
+                    type: 'Feature',
+                    geometry: {
+                        type: 'Point',
+                        coordinates: end
+                    }
+                }
+            }
+        });
+// this is where the JavaScript from the next step will go
+    });
+}
